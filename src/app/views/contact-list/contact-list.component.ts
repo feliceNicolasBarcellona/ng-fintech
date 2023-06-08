dbf5be676ef2fdf763865cdd1dc1d20a98062bf6
Felice Nicolas Barcellona <felicebarcellona1@gmail.com>
2023-06-08 16:28:48 +0200

    add contact-list component

---
+++ src/app/views/contact-list/contact-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Contact } from 'src/app/models/contact';
+
+@Component({
+  selector: 'app-contact-list',
+  templateUrl: './contact-list.component.html',
+  styleUrls: ['./contact-list.component.scss'],
+})
+export class ContactListComponent {
+
+  @Input() contacts: Contact[] = [];
+  @Output() selectContact: EventEmitter<Contact> = new EventEmitter();
+  @Output() editContact: EventEmitter<Contact> = new EventEmitter();
+  @Output() removeContact: EventEmitter<Contact> = new EventEmitter();
+
+}
--- src/app/views/contacts/contacts.component.ts
+++ src/app/views/contacts/contacts.component.ts
@@ -1,5 +1,7 @@
+import { Contact } from './../../models/contact';
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-contacts',
@@ -7,5 +9,26 @@ import { MatDialogRef } from '@angular/material/dialog';
   styleUrls: ['./contacts.component.scss']
 })
 export class ContactsComponent {
+
+  contacts: Contact[]= [
+    {_id: '1', name: 'Michele', surname:'Stieven', iban: '1111111111111111'},
+    {_id: '2', name: 'Fabio', surname:'Biondi', iban: '1234123412341234'},
+  ]
+
   constructor(public dialogRef: MatDialogRef<ContactsComponent>) {}
+
+  selectContact(contact: Contact){
+    console.log(contact._id);
+  }
+
+  editContact(contact: Contact){
+    console.log(contact._id);
+  }
+
+  removeContact(contact: Contact){
+    console.log(contact._id);
+
+  }
+
+
 }
